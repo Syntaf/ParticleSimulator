816db74d582dea0ccd8384561d78997039a3ec58
Grant Mercer <Gmercer015@gmail.com>
2014-09-07 13:57:06 -0700

    added pragma comment back

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@
 #endif
 
 #ifdef WIN32
-//#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 #endif
 
 int main(int argc, char* argv[]) {
