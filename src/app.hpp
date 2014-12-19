2e16ef155f3bad0ecb32bb08cc958ce5acb46fc9
Grant Mercer <Gmercer015@gmail.com>
2014-12-18 19:08:32 -0800

    Documentation for app class written

--- src/app.hpp
+++ src/app.hpp
@@ -9,54 +9,54 @@
 class App{
 
     public:
+        // Constructor simply creates main window, specify context settings
+        //	 and set position of main window to center of screen	
         App(sf::VideoMode mode);
         ~App();
 
-        //initialize uniform locations, compile shaders and create
-        //  program ID
+        // Initialize all OpenGL aspects of program, ensures GLEW is working
+        //	 and allocates the particle manager. Once particle manager is
+        //	 allocated the console window is able to be binded and does such,
+        //	 then the openGL buffers are generated, shaders loaded and shader
+        //	 uniform locations created.
         bool initGL(const int& particle_count);
 
-        //load a texture and initialize particle starting positions
-        //  if openCL is enabled it will also put all of the new data
-        //  into a buffer for the GPU to use.
+        // Load texture and internally initialize all particles once the texture
+        //	 is loaded by calling the particle manager init particles. See
+        //	 Particle Manager initParticles for more detailed information.
         bool initParticles(const std::string& texturepath);
 
-        //the main program loop, run will handle event, receive user input
-        //  and handle user input. the most important part of the class
+        // The main loop of the program, while active, run calculates the delta time
+        //	 for game movement, handles window events for both the console and window,
+        //	 grabs matrices based off user input and makes calls to particle manager
+        //	 rendering the particles to the screen.
         void run();
 
-        //this is an exit method to be used for the console window, specifically to set running
-        //to false
+        // Exit method created for use by the console window with an exit event is 
+        //	 parsed by the console. If the console closes so does the program
         static void procClose();
 
     private:
-        //main window to render to
-        sf::Window d_main_window;
+        sf::Window d_main_window;               // main window for program.
 
-        //we need to control when these are created due to the nature of their
-        //  constructors, so we will use dynamic allocations.
 #ifdef USE_TGUI
-        ConsoleManager *d_console_window;
+        ConsoleManager *d_console_window;       // console window for parsing commands
 #endif
-        ParticleManager *d_particles_manager;
+        ParticleManager *d_particles_manager;   // manages all aspects of particles
 
-        //OpenGL variables
-        GLuint d_program_id;
-        GLuint d_cameraright_worldspace_id;
-        GLuint d_cameraup_worldspace_id;
-        GLuint d_viewprojmatrix_id;
+        GLuint d_program_id;                    // we will only be using one program
+        GLuint d_cameraright_worldspace_id;     // vector used by shader
+        GLuint d_cameraup_worldspace_id;        // another vector used by shader
+        GLuint d_viewprojmatrix_id;             // view and projection matrix for shader
 
-        //texture ID
-        GLuint d_texture_id;
+        GLuint d_texture_id;                    // holds id of our texture
 
-        //timers for delta and FPS
-        sf::Clock clock;
-        sf::Clock fps_clock;
+        sf::Clock clock;                        // calculates delta time
+        sf::Clock fps_clock;                    // calculates fps
 
-        //loop variables
-        static bool running;
-        bool pressed;
-        float fps_last_time;
+        static bool running;                    // application runs while this is true
+        bool pressed;                           // is the mouse pressed?
+        float fps_last_time;                    // var for calculating fps
 };
 
 #endif
