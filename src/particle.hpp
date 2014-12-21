d17e9c30da6fff6a08957ca550ffb83035e2be8c
Grant Mercer <Gmercer015@gmail.com>
2014-12-20 18:04:47 -0800

    fixed formatting issues in particle

--- src/particle.hpp
+++ src/particle.hpp
@@ -5,20 +5,20 @@
 #include <vector>
 
 struct Particle{
-	// Add a single force to the particle, this could
-	//	 be anything from drag to the mouseforce applied
-	//	 to each particle. Note that this is NOT setting
-	//	 the speed of the particle.
+    // Add a single force to the particle, this could
+    //	 be anything from drag to the mouseforce applied
+    //	 to each particle. Note that this is NOT setting
+    //	 the speed of the particle.
     void addForce(glm::vec3 const& m_force)
     {
         force.push_back(m_force);
     }
 
-	// Sums up the total force of the force vector and 
-	//	 returns a vec3 of the result. This will be used
-	//	 in calculating the position and speed of the 
-	//	 particle by determing the total forces acting
-	//	 on it.
+    // Sums up the total force of the force vector and 
+    //	 returns a vec3 of the result. This will be used
+    //	 in calculating the position and speed of the 
+    //	 particle by determing the total forces acting
+    //	 on it.
     glm::vec3 getTotalForce() const
     {
         glm::vec3 total(0.0,0.0,0.0);
@@ -28,16 +28,16 @@ struct Particle{
         return total;
     }
 
-	// Clear force vector, essentially 'stopping' a particle
+    // Clear force vector, essentially 'stopping' a particle
     void clearForce()
     {
         force.clear();
     }
 
-	glm::vec3 pos, speed;            // position and speed of a particle represented as vec3's
+    glm::vec3 pos, speed;            // position and speed of a particle represented as vec3's
     std::vector<glm::vec3> force;    // list of forces acting on a particle
     unsigned char r,g,b,a;           // specific color of particle
-    float size;						 // how large each particle is
+    float size;                      // how large each particle is
     float life;                      // remaining life of a particle
     float cameradistance;            // squared distance to camera : -1.0f if dead
 };
