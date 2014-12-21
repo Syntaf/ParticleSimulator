4506a7a16bcf6dabcb70330b99c2712a49b4ee36
Grant Mercer <Gmercer015@gmail.com>
2014-12-20 18:03:34 -0800

    refactored particle struct and wrote Documentation for it

--- src/particle.hpp
+++ src/particle.hpp
@@ -4,20 +4,21 @@
 #include <glm/glm.hpp>
 #include <vector>
 
-// CPU representation of a particle
 struct Particle{
-    glm::vec3 pos, speed;            //position, speed
-    std::vector<glm::vec3> force;    //total force
-    unsigned char r,g,b,a;            //color
-    float size;
-    float life;                        //remaining life of a particle, if <0 it's super dead
-    float cameradistance;            //squared distance to camera : -1.0f if dead
-
+	// Add a single force to the particle, this could
+	//	 be anything from drag to the mouseforce applied
+	//	 to each particle. Note that this is NOT setting
+	//	 the speed of the particle.
     void addForce(glm::vec3 const& m_force)
     {
         force.push_back(m_force);
     }
 
+	// Sums up the total force of the force vector and 
+	//	 returns a vec3 of the result. This will be used
+	//	 in calculating the position and speed of the 
+	//	 particle by determing the total forces acting
+	//	 on it.
     glm::vec3 getTotalForce() const
     {
         glm::vec3 total(0.0,0.0,0.0);
@@ -27,15 +28,18 @@ struct Particle{
         return total;
     }
 
+	// Clear force vector, essentially 'stopping' a particle
     void clearForce()
     {
         force.clear();
     }
 
-    //used for std::sort, needs an overloaded comparison operator
-    bool operator<(const Particle& that) const {
-        return this->cameradistance > that.cameradistance;
-    }
+	glm::vec3 pos, speed;            // position and speed of a particle represented as vec3's
+    std::vector<glm::vec3> force;    // list of forces acting on a particle
+    unsigned char r,g,b,a;           // specific color of particle
+    float size;						 // how large each particle is
+    float life;                      // remaining life of a particle
+    float cameradistance;            // squared distance to camera : -1.0f if dead
 };
 
 #endif
